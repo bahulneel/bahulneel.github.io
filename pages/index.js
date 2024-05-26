f0d84d75fc459af2392fcf3a30a380cbb46c6b7e
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2024-05-26 22:24:16 +0100

    pallete and layout

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 // Home Page content
 import Head from 'next/head';
 
@@ -8,10 +7,14 @@ function Page() {
       <Head>
         <title>Home</title>
       </Head>
-      <h1 className="text-3xl font-bold mb-4">Welcome to My Professional Portfolio</h1>
-      <img src="/profile.webp" alt="Professional Photo" className="w-32 h-32 rounded-full mb-4"/>
-      <p className="mb-4">Seasoned software architect and developer with over 20 years of experience in building large-scale, innovative software solutions. Adept at data platform development, web systems, and enhancing team performance.</p>
-      <p>Regularly engages with academic research and industry trends to stay at the cutting edge of technology. Committed to promoting neurodiversity in the workplace.</p>
+      <div className="bg-gradient-to-r from-gradient-start to-gradient-end p-4 rounded-md shadow-md">
+        <h1 className="text-3xl font-bold mb-4 text-accent-white">Welcome to My Professional Portfolio</h1>
+      </div>
+      <div className="bg-primary-darkBlue p-4 rounded-md shadow-md mt-4">
+        <img src="/profile.webp" alt="Professional Photo" className="w-32 h-32 rounded-full mb-4" />
+        <p className="mb-4 text-accent-white">Seasoned software architect and developer with over 20 years of experience in building large-scale, innovative software solutions. Adept at data platform development, web systems, and enhancing team performance.</p>
+        <p className="text-accent-white">Regularly engages with academic research and industry trends to stay at the cutting edge of technology. Committed to promoting neurodiversity in the workplace.</p>
+      </div>
     </div>
   );
 }
--- pages/skills.js
+++ pages/skills.js
@@ -1,27 +1,26 @@
-
 // Skills content
 import Head from 'next/head';
 
 function Page() {
   return (
-    <div className="container mx-auto px-4 py-6">
+    <div className="container mx-auto px-4 py-6 bg-secondary-lightGray">
       <Head>
         <title>Skills</title>
       </Head>
-      <h1 className="text-3xl font-bold mb-4">Professional Skills</h1>
-      <h2 className="text-2xl font-semibold mb-3">Technical Skills</h2>
+      <h1 className="text-3xl font-bold mb-4 text-primary-darkBlue">Professional Skills</h1>
+      <h2 className="text-2xl font-semibold mb-3 text-primary-darkBlue">Technical Skills</h2>
       <ul className="list-disc ml-5 mb-6">
-        <li>Advanced knowledge in JavaScript, TypeScript, and Node.js</li>
-        <li>Proficient in data architecture and management with MySQL, Postgres, and MongoDB</li>
-        <li>Experienced in building and maintaining web applications using React, Vue, and Next.js</li>
-        <li>Skilled in machine learning and AI technologies, including TensorFlow and custom GPT models</li>
+        <li className="text-secondary-gray">Advanced knowledge in JavaScript, TypeScript, and Node.js</li>
+        <li className="text-secondary-gray">Proficient in data architecture and management with MySQL, Postgres, and MongoDB</li>
+        <li className="text-secondary-gray">Experienced in building and maintaining web applications using React, Vue, and Next.js</li>
+        <li className="text-secondary-gray">Skilled in machine learning and AI technologies, including TensorFlow and custom GPT models</li>
       </ul>
-      <h2 className="text-2xl font-semibold mb-3">Soft Skills</h2>
+      <h2 className="text-2xl font-semibold mb-3 text-primary-darkBlue">Soft Skills</h2>
       <ul className="list-disc ml-5">
-        <li>Strong leadership and team management abilities</li>
-        <li>Excellent communication and interpersonal skills</li>
-        <li>Proven problem-solving and analytical capabilities</li>
-        <li>Commitment to continuous learning and professional development</li>
+        <li className="text-secondary-gray">Strong leadership and team management abilities</li>
+        <li className="text-secondary-gray">Excellent communication and interpersonal skills</li>
+        <li className="text-secondary-gray">Proven problem-solving and analytical capabilities</li>
+        <li className="text-secondary-gray">Commitment to continuous learning and professional development</li>
       </ul>
     </div>
   );
