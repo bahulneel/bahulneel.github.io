b4ceccd4ee82f6beec7a9534524e112a22ae01c9
Bahul Neel Upadhyaya <bahul.upadhyaya@contxt.id>
2024-07-02 14:30:50 +0100

    Fixed date helper usage

--- scripts/convert_cv.js
+++ scripts/convert_cv.js
@@ -1,32 +1,27 @@
 // Import required modules
 const fs = require('fs');
 const Handlebars = require('handlebars');
+const { parseISO, format } = require('date-fns');
 console.log('Writing to cv.adoc');
 
 // Read the CV JSON data
 const cvData = JSON.parse(fs.readFileSync('public/cv.json', 'utf8'));
 
-// Helper to format date
-function formatDate() {
-  return function (dateStr, render) {
-
-    if (!dateStr) {
-      return 'Present';
-    }
-    const date = format(parseISO(render(dateStr)), 'MMMM yyyy');
-    return date;
+// Register a helper to format date
+Handlebars.registerHelper('formatDate', function (dateStr) {
+  if (!dateStr) {
+    return 'Present';
   }
-}
+  const date = format(parseISO(dateStr), 'MMMM yyyy');
+  return date;
+});
 
 // Load the Handlebars template from cv_template.mustache
 const template = fs.readFileSync('scripts/cv_template.mustache', 'utf8');
 const handlebarsTemplate = Handlebars.compile(template);
 
-// Render the template with the CV data and custom formatting for dates
-const output = handlebarsTemplate({
-  ...cvData,
-  formatDate
-});
+// Render the template with the CV data
+const output = handlebarsTemplate(cvData);
 
 // Write the output to cv.adoc
 fs.writeFileSync('public/cv.adoc', output);
